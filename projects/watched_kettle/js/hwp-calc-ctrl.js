f4b5eb1c38bdfd957eeb729e0aaea0d552dea617
Joe Desbonnet <jdesbonnet@gmail.com>
2016-08-07 21:17:38 +0100

    add reset button to hot water project

--- projects/watched_kettle/js/hwp-calc-ctrl.js
+++ projects/watched_kettle/js/hwp-calc-ctrl.js
@@ -6,25 +6,35 @@ hwpapp.controller('hwpCalcCtrl',['$scope', '$interval', function($scope, $interv
 
     //$scope.time = ($scope.volume/1000) * 4200 * ($scope.target_temperature - $scope.start_temperature) / ($scope.power*1000);
 
+    var intervalTimer;
 
     $scope.startCountdownTimer = function () {
 
+	$scope.timerRunning = true;
 
         var time = ($scope.volume/1000) * 4200 * ($scope.target_temperature - $scope.start_temperature) / ($scope.power*1000);
         $scope.countdown_timer = Math.ceil(time);
 
         var start_time = new Date().getTime();
 
-        var interval = $interval(function() {
+        intervalTimer = $interval(function() {
             $scope.countdown_timer--;
             var now = new Date().getTime();
             var temperature = $scope.start_temperature + $scope.power * 1000 * (now - start_time) / (4200 * $scope.volume);
             $scope.current_temperature = temperature.toFixed(1);
             if ($scope.countdown_timer == 0) {
-                $interval.cancel(interval);
+		$scope.timerRunning = false;
+                $interval.cancel(intervalTimer);
 		beep();
             }
         }, 1000);
 
+    };
+
+
+    $scope.resetCountdownTimer = function () {
+        $scope.timerRunning = false;
+	$interval.cancel(intervalTimer);
+	$scope.countdown_timer = 0;
     };
 }]);
